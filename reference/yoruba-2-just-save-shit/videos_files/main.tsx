2120001dc47ce3dbe5fcb7569574dd16f68286d0
KingWilliamsGPT <williamusanga23@gmail.com>
2026-07-08 07:50:21 +0100

    first commit

---
+++ reference/yoruba-2-just-save-shit/videos_files/main.tsx
@@ -0,0 +1,12 @@
+"use strict";
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=9adf64ea"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=9adf64ea"; const createRoot = __vite__cjsImport1_reactDom_client["createRoot"];
+import App from "/src/App.tsx";
+import "/src/index.css";
+createRoot(document.getElementById("root")).render(/* @__PURE__ */ jsxDEV(App, { "data-replit-metadata": "artifacts/toddler-vocab/src/main.tsx:5:52", "data-component-name": "App" }, void 0, false, {
+  fileName: "/home/runner/workspace/artifacts/toddler-vocab/src/main.tsx",
+  lineNumber: 5,
+  columnNumber: 53
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IjtBQUlvRDtBQUpwRCxTQUFTQSxrQkFBa0I7QUFDM0IsT0FBT0MsU0FBUztBQUNoQixPQUFPO0FBRVBELFdBQVdFLFNBQVNDLGVBQWUsTUFBTSxDQUFFLEVBQUVDLE9BQU8sdUJBQUMsT0FBRSxxR0FBSDtBQUFBO0FBQUE7QUFBQTtBQUFBLE9BQUksQ0FBRyIsIm5hbWVzIjpbImNyZWF0ZVJvb3QiLCJBcHAiLCJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIiwicmVuZGVyIl0sImlnbm9yZUxpc3QiOltdLCJzb3VyY2VzIjpbIm1haW4udHN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IGNyZWF0ZVJvb3QgfSBmcm9tIFwicmVhY3QtZG9tL2NsaWVudFwiO1xuaW1wb3J0IEFwcCBmcm9tIFwiLi9BcHBcIjtcbmltcG9ydCBcIi4vaW5kZXguY3NzXCI7XG5cbmNyZWF0ZVJvb3QoZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoXCJyb290XCIpISkucmVuZGVyKDxBcHAgLz4pO1xuIl0sImZpbGUiOiIvaG9tZS9ydW5uZXIvd29ya3NwYWNlL2FydGlmYWN0cy90b2RkbGVyLXZvY2FiL3NyYy9tYWluLnRzeCJ9
